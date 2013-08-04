4e7fd6fdc0d3ec987e0f08d1c5130f871645ff1e
GitMongo user <GitMongo email>
2013-08-04 14:57:51 +0200

    added basic folder structure and very rough outline of front controller

---
+++ classes/Plista/Orchestra/Orchestra.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This is the front controller for the Orchestra remote execution and messaging framework.
+ */
+class Orchestra {
+	/**
+	 * This function is used to queue a Task for remote execution.
+	 * @param Task $task the Task to be executed. Tasks consist of a Jobname and parameters.
+	 * @return bool whether or not the queueing was successful
+	 */
+	public function queue(Task $task) {
+	
+	}
+	
+	public function 
+
+}
