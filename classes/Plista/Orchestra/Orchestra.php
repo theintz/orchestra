abc9ab51479962ff5e99d43241e46185dd1b3b29
GitMongo user <GitMongo email>
2013-08-04 14:58:22 +0200

    added namespace for front controller

--- classes/Plista/Orchestra/Orchestra.php
+++ classes/Plista/Orchestra/Orchestra.php
@@ -5,7 +5,7 @@
  */
 class Orchestra {
 	/**
-	 * This function is used to queue a Task for remote execution.
+	 * This method is used to queue a Task for remote execution.
 	 * @param Task $task the Task to be executed. Tasks consist of a Jobname and parameters.
 	 * @return bool whether or not the queueing was successful
 	 */
@@ -13,6 +13,12 @@ class Orchestra {
 	
 	}
 	
-	public function 
-
+    /**
+     * This method is used to deliver a message to all subscribed recipients.
+     * @param Message $msg the Message to be delivered. Since Orchestra works on a subscriber model, the Message does not contain any explicit recipients, but rather parameter that recipients can subscribe to.
+     * @return bool whether or not the delivery was successful
+     */
+	public function deliver(Message $msg) {
+    
+    }
 }
